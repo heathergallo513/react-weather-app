25b14c2d67ff3193e659e5f277cfa4c1936e9d68
heathergallo513 <128610780+heathergallo513@users.noreply.github.com>
2023-07-31 21:05:54 -0400

    Added GitHub and personal URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/heather-gallo3/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Heather Gallo{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/heathergallo513/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
