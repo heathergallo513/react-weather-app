13b80d7d6e4c8695de6e12c5b404ab2b74bd0abc
heathergallo513 <128610780+heathergallo513@users.noreply.github.com>
2023-08-01 22:15:34 -0400

    Added basic styling CSS

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,19 +28,27 @@ export default function Weather() {
         <li>Wednesday 9:30 PM</li>
         <li>Sunny</li>
       </ul>
-      <div className="col-6">
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-          alt="sunny"
-        />
-        6℃
-      </div>
-      <div className="col-6">
-        <ul>
-          <li>Precipitation: 15%</li>
-          <li>Humidity: 72%</li>
-          <li>Wind: 13 km/h</li>
-        </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sunny"
+              className="float-left"
+            />
+            <div className="float-left">
+              <span className="temperature">6</span>
+              <span className="units">℃</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
