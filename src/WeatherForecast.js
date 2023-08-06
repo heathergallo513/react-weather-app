bfdaa1ddc2b804b60e57f83a563d3accb6d4ff8a
heathergallo513 <128610780+heathergallo513@users.noreply.github.com>
2023-08-06 10:19:36 -0400

    Integrated lat and lon into applicable functions

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
